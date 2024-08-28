83fa182546df6d1f102aed7a58196430774966a4
Yogie Yadav <yogiejay21@gmail.com>
2024-08-29 01:40:55 +0530

    Implemented skeleton for footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./app/component/footer/footer";
 import Header from "./app/component/header/header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
